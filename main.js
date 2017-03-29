d00920ee2fd7800285d75468800944e56181d5b7
Sandeep Suresh <sureshsa@usc.edu>
2017-03-29 14:16:01 -0700

    Updated the button bar and I have added some features with clicking the link.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function showUSC()
+function uscClicked()
 {
-	console.log("Hi");
+	window.location.href = "https://github.com/Sandeepsuresh1998";
+}
+function resourcesClicked()
+{
+	window.location.href = "https://docs.google.com/document/d/1BFNgxos5S5S22WXlpWCK1e9qYphD4VF5XVfRrMNXXAA/edit";
 }
