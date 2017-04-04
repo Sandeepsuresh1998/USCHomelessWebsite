a45cf5650240aae270a9a510eb5642931e267fa0
Sandeep Suresh <sureshsa@usc.edu>
2017-04-03 19:08:01 -0700

    Updated links to the website. Five tabs that link to the different pages we need.

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-function uscClicked()
-{
-	window.location.href = "https://github.com/Sandeepsuresh1998";
-}
-function resourcesClicked()
+function uscClicked() //Needs to be changed to the webpage for specifically the USC resources
 {
 	window.location.href = "https://docs.google.com/document/d/1BFNgxos5S5S22WXlpWCK1e9qYphD4VF5XVfRrMNXXAA/edit";
+}
+function laResourcesClicked() //Needs to be changed to the webpage for specifically the LA resources
+{
+	window.location.href = "https://docs.google.com/document/d/1BFNgxos5S5S22WXlpWCK1e9qYphD4VF5XVfRrMNXXAA/edit";
+}
+function nationalClicked() //Needs to be changed to the webpage for specifically the national resources
+{
+	window.location.href = "https://docs.google.com/document/d/1BFNgxos5S5S22WXlpWCK1e9qYphD4VF5XVfRrMNXXAA/edit";
+}
+function surveyClicked()
+{
+	window.location.href = "https://goo.gl/forms/KTTlMxQ0CKwyab1l2";
+}
+function storyClicked() //Needs to be updated with the correct link to the survey
+{
+	window.location.href = "https://goo.gl/forms/nQWjPLnmUgjbbYww2";
 }
