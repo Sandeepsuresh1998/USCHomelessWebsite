d67054e8ee9a6535dbdbcd9eda1601b6dc5d7cd7
Sandeep Suresh <sureshsa@usc.edu>
2017-04-09 09:16:56 -0700

    Buttons are updated.

--- main.js
+++ main.js
@@ -18,3 +18,13 @@ function storyClicked() //Needs to be updated with the correct link to the surve
 {
 	window.location.href = "https://goo.gl/forms/nQWjPLnmUgjbbYww2";
 }
+/* Open when someone clicks on the span element */
+function openNav() {
+	console.log("I'm getting called");
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
