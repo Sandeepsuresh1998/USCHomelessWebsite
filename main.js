4d6d6fcba10c0e28466114d186701966a17ba5d8
Sandeep Suresh <sureshsa@usc.edu>
2017-04-10 23:08:28 -0700

    Added a new html for the new window that stores all the usc resources.

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-function uscClicked() //Needs to be changed to the webpage for specifically the USC resources
-{
-	window.location.href = "https://docs.google.com/document/d/1BFNgxos5S5S22WXlpWCK1e9qYphD4VF5XVfRrMNXXAA/edit";
-}
-function laResourcesClicked() //Needs to be changed to the webpage for specifically the LA resources
-{
-	window.location.href = "https://docs.google.com/document/d/1BFNgxos5S5S22WXlpWCK1e9qYphD4VF5XVfRrMNXXAA/edit";
-}
-function nationalClicked() //Needs to be changed to the webpage for specifically the national resources
-{
-	window.location.href = "https://docs.google.com/document/d/1BFNgxos5S5S22WXlpWCK1e9qYphD4VF5XVfRrMNXXAA/edit";
-}
 function surveyClicked()
 {
 	window.location.href = "https://goo.gl/forms/KTTlMxQ0CKwyab1l2";
@@ -20,11 +8,10 @@ function storyClicked() //Needs to be updated with the correct link to the surve
 }
 /* Open when someone clicks on the span element */
 function openNav() {
-	console.log("I'm getting called");
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("myNav").style.height = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("myNav").style.height = "0%";
 }
